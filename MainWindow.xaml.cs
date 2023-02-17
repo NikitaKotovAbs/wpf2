9e1c0ade8024a5d7f26c0357167eb66ae374dc9e
NikitaKotovAbs <112644423+NikitaKotovAbs@users.noreply.github.com>
2023-02-18 00:49:26 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Diary2.Models;
+
+namespace Diary2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private BindingList<Class1> todoData;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            todoData = new BindingList<Class1>()
+            {
+                new Class1(){Text = "test"},
+                new Class1(){Text = "cheto"}
+            };
+        }
+    }
+}
---
+++ Models/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diary2.Models
+{
+    class Class1
+    {
+        internal string Text;
+
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+         
+    }
+}
